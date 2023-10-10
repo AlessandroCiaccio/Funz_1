4484d8ba96eb72d64db49758fe6f421e6e16b6dc
Alessandro Scrofani <sandroscro@libero.it>
2023-10-10 19:03:20 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String [] arg){
+        int a=18;
+        int b=22;
+        System.out.print(Sum(a,b));
+    }
+    public static int Sum(int x, int y){
+        return (x+y);
+        
+    }
+}
